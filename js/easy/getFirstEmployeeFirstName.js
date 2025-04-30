e3387447282e4d624ccccaaa6debd2f4092e8fc9
Amal Issellay <amalissellay01@gmail.com>
2025-04-30 15:27:18 +0100

    Fix: correct employee first name retrieval and add missing employee entry

--- js/easy/getFirstEmployeeFirstName.js
+++ js/easy/getFirstEmployeeFirstName.js
@@ -1,5 +1,6 @@
 const employees = 
 [
+    {f_nmae: "jannat", l_name: "issellay"},
     {f_nmae: "Amal", l_name: "issellay"},
     {f_nmae: "hdsuh", l_name: "dksjcjc"},
     {f_nmae: "sidjoj", l_name: "fujiodsh"},
@@ -9,7 +10,7 @@ const employees =
 function getFIrstEmployeeFirstNAme(employees)
 {
     const firstEmployee = employees[0];
-    return firstEmployee.f_name;
+    return firstEmployee.f_nmae;
 }
 
-console.log(getFIrstEmployeeFirstNAme);
+console.log(getFIrstEmployeeFirstNAme(employees));
